ae7f65913f260b5f3f9f95b413c83d27fc65d290
AxMari <AlexisMarien@gmail.com>
2016-06-07 20:30:14 -0400

    mapdemo

--- Map.js
+++ Map.js
@@ -8,7 +8,7 @@ var MapFcns = {
             airportList.html('');
             airportList.append('<option value=""></option>');
         for (var i in sites) {
-            var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
+            var newOption = $('<option value="' + sites[i].Code + '">' + '(' + sites[i].Code + ") " + sites[i].City + ", " + sites[i].State  + '</option>');
             airportList.append(newOption);
         }
     },
@@ -19,17 +19,27 @@ var MapFcns = {
             if(airportCode) {
                 var currAirport = _.findWhere(sites, {Code: airportCode});
                 $('#setting-code').text('(' + currAirport.Code + ') ' + currAirport.PlainName);
-                $('#setting-city').text(currAirport.City + ", " +currAirport.State);
                 $('#setting-longlat').text("(" + currAirport.Longitude + ", " + currAirport.Latitude + ")");
                 
                 var marker = new google.maps.Marker({
                     position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
                     map: globalMap,
-                    title: currAirport.Code
+                    title: "(" + currAirport.Code + ") double click to remove" 
                 });
+                globalMap.setZoom(6);
+                globalMap.panTo(marker.position);
+                google.maps.event.addListener(marker, 'dblclick', function() {
+                marker.setVisible(false);
+                });
+
             }
+
     }
-}
+
+};
+
+
+
 
 
 MapFcns.loadSiteList();
@@ -60,7 +70,8 @@ function  initMap() {
   globalMap = new google.maps.Map(document.getElementById('airport-map'), {
     center: {lat: 42.2814, lng: -83.7483},
     scrollwheel: true,
-    zoom: 6
+    zoom: 4
+
   });
   
-    }
+};
